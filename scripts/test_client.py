1c37a175226131578c2a6733316422a0a0d40812
Radu Popescu <mail@radupopescu.net>
2018-01-12 15:35:37 +0100

    Cleaning up test scripts

--- scripts/test_client.py
+++ scripts/test_client.py
@@ -7,20 +7,51 @@
 import asyncio
 import json
 import websockets
+from optparse import OptionParser
 
-async def hello(uri, frame):
+async def hello(uri, frame, verbose):
     async with websockets.connect(uri) as websocket:
-        print("Frame sent: {}".format(frame))
+        if verbose:
+            print("Subscription request sent")
+
         await websocket.send(frame)
         reply = await websocket.recv()
-        print("Frame received: {}".format(json.loads(reply.decode())))
-        # try:
-        #     pong = await websocket.ping()
-        #     await asyncio.wait_for(pong, timeout=10)
-        # except asyncio.TimeoutError:
-        #     print("Timeout reached while waiting for pong")
-        # print("Pong received")
 
-subscription_msg = json.dumps({'repo' : 'test_repo', 'min_revision' : 0 })
-asyncio.get_event_loop().run_until_complete(
-    hello('ws://localhost:8081/api/v1/notify', str.encode(subscription_msg)))
+        if verbose:
+            print("Notification received: {}".format(json.loads(reply.decode())))
+        else:
+            print(reply.decode())
+
+def main():
+    usage = "{}\n\n{}".format("Usage: %prog [options] URL REPO_NAME MIN_REVISION",
+                              "Ex: %prog localhost:8081/api/v1/notify my_repo 17 abcdef")
+    parser = OptionParser(usage)
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
+                      help="verbose output")
+    parser.add_option("-o", "--one-shot", dest="one_shot", action="store_true",
+                      help="one-shot mode: exit after the first notification "
+                      + "is received or timeout is reached")
+
+    (options, args) = parser.parse_args()
+    if len(args) != 3 :
+        parser.error("incorrect number of arguments")
+
+    url = args[0]
+    repo_name = args[1]
+    min_revision = args[2]
+
+    if options.verbose:
+        print("Configuration:")
+        print("  notification service URL: {}".format(url))
+        print("  repository name: {}".format(repo_name))
+        print("  min revision: {}".format(min_revision))
+        print()
+
+    subscription_msg = json.dumps({'repo' : repo_name,
+                                   'min_revision' : min_revision })
+    asyncio.get_event_loop().run_until_complete(
+        hello('ws://' + url, str.encode(subscription_msg), options.verbose)
+    )
+
+if __name__ == '__main__':
+    main()
--- scripts/test_trigger.py
+++ scripts/test_trigger.py
@@ -4,26 +4,51 @@
 ### This file is part of the CernVM File System.
 ###-------------------------------------------------------------------
 
+from optparse import OptionParser
 import requests
 
-host_name = 'localhost'
-port = 8081
-api_root = '/api/v1'
+def do_trigger(url, repo, revision, root_hash, verbose):
+    if verbose:
+        print('Trigger: ', repo, revision, root_hash)
 
-url = 'http://' + host_name + ':' + str(port) + api_root + '/trigger'
-
-def do_trigger(repo, revision, root_hash):
-    print('Trigger: ', repo, revision, root_hash)
     data = {'repo' : repo,
             'revision' : revision,
             'root_hash' : root_hash,
             'api_version' : 1}
     headers = {'Content-type' : 'application/json'}
-    reply = requests.post(url, json = data, headers = headers)
-    print('Reply: status: {}, json: {}'.format(reply.status_code, reply.json()))
+    reply = requests.post('http://' + url, json = data, headers = headers)
+
+    if verbose:
+        print('Reply: status: {}, json: {}'.format(reply.status_code, reply.json()))
+    else:
+        print(reply.json())
 
 def main():
-    do_trigger('test_repo', 5, 'abcdefg')
+    usage = "{}\n\n{}".format("Usage: %prog [options] URL REPO_NAME REVISION ROOT_HASH",
+                            "Ex: %prog localhost:8081/api/v1/trigger my_repo 17 abcdef")
+
+    parser = OptionParser(usage)
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
+                      help="verbose output")
+
+    (options, args) = parser.parse_args()
+    if len(args) != 4 :
+        parser.error("incorrect number of arguments")
+
+    url = args[0]
+    repo_name = args[1]
+    revision = args[2]
+    root_hash = args[3]
+
+    if options.verbose:
+        print("Configuration:")
+        print("  notification service URL: {}".format(url))
+        print("  repository name: {}".format(repo_name))
+        print("  revision: {}".format(revision))
+        print("  root hash: {}".format(root_hash))
+        print()
+
+    do_trigger(url, repo_name, revision, root_hash, options.verbose)
 
 if __name__ == '__main__':
     main()
