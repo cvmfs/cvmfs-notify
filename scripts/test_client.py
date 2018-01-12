4271336bacb4f7dc0d0c18ab5e29d17739d6be82
Radu Popescu <mail@radupopescu.net>
2018-01-12 17:24:38 +0100

    Improve test_client.py
    * Enable continuous mode
    * Misc cleanup

--- scripts/test_client.py
+++ scripts/test_client.py
@@ -9,8 +9,8 @@ import json
 import websockets
 from optparse import OptionParser
 
-async def hello(uri, frame, verbose):
-    async with websockets.connect(uri) as websocket:
+async def do_request(url, frame, verbose):
+    async with websockets.connect(url) as websocket:
         if verbose:
             print("Subscription request sent")
 
@@ -22,14 +22,26 @@ async def hello(uri, frame, verbose):
         else:
             print(reply.decode())
 
+        return reply.decode()
+
+def wait_for_update(url, repo_name, min_revision, verbose):
+    msg = str.encode(json.dumps({'repo' : repo_name,
+                                 'min_revision' : min_revision }))
+    reply = asyncio.get_event_loop().run_until_complete(
+        do_request('ws://' + url, msg, verbose)
+    )
+
+    return json.loads(reply)
+
+
 def main():
     usage = "{}\n\n{}".format("Usage: %prog [options] URL REPO_NAME MIN_REVISION",
                               "Ex: %prog localhost:8081/api/v1/notify my_repo 17 abcdef")
     parser = OptionParser(usage)
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
                       help="verbose output")
-    parser.add_option("-o", "--one-shot", dest="one_shot", action="store_true",
-                      help="one-shot mode: exit after the first notification "
+    parser.add_option("-c", "--continuous", dest="continuous", action="store_true",
+                      help="continuous mode: do not exit after the first notification "
                       + "is received or timeout is reached")
 
     (options, args) = parser.parse_args()
@@ -38,7 +50,7 @@ def main():
 
     url = args[0]
     repo_name = args[1]
-    min_revision = args[2]
+    min_revision = int(args[2])
 
     if options.verbose:
         print("Configuration:")
@@ -47,11 +59,17 @@ def main():
         print("  min revision: {}".format(min_revision))
         print()
 
-    subscription_msg = json.dumps({'repo' : repo_name,
-                                   'min_revision' : min_revision })
-    asyncio.get_event_loop().run_until_complete(
-        hello('ws://' + url, str.encode(subscription_msg), options.verbose)
-    )
+    current_revision = min_revision
+    reply = wait_for_update(url, repo_name, min_revision, options.verbose)
+
+    print("New status: {}".format(reply))
+
+    if options.continuous:
+        while True:
+            if 'revision' in reply:
+                current_revision = reply['revision'] + 1;
+            reply = wait_for_update(url, repo_name, current_revision,
+                                    options.verbose)
 
 if __name__ == '__main__':
     main()
