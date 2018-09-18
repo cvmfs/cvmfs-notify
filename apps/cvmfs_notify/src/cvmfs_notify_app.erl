6eada4c2370dbd635e549c6ecc32a5e6e2f83dfc
Radu Popescu <mail@radupopescu.net>
2018-09-19 00:16:20 +0200

    Provide AMQP credentials as environment variables

--- apps/cvmfs_notify/src/cvmfs_notify_app.erl
+++ apps/cvmfs_notify/src/cvmfs_notify_app.erl
@@ -1,6 +1,9 @@
 %%%-------------------------------------------------------------------
-%% @doc cvmfs_notify public API
-%% @end
+%%% This file is part of the CernVM File System.
+%%%
+%%% @doc cvmfs_notify application
+%%%
+%%% @end
 %%%-------------------------------------------------------------------
 
 -module(cvmfs_notify_app).
@@ -19,12 +22,27 @@
 start(_StartType, _StartArgs) ->
     UserVars = util:read_vars(user_config, util:default_config()),
 
-    TcpPort = maps:get(port, UserVars),
+    AMQPUser = os:getenv("CVMFS_NOTIFY_AMQP_USERNAME"),
+    AMQPPass = os:getenv("CVMFS_NOTIFY_AMQP_PASSWORD"),
+
+    case lists:any(fun(V) -> (V =:= false) or (V =:= []) end, [AMQPUser, AMQPPass]) of
+        true ->
+            lager:error("AMQP credentials not found. " ++
+                        "Please set the CVMFS_NOTIFY_AMQP_USERNAME and " ++
+                        "CVMFS_NOTIFY_AMQP_PASSWORD environment variables"),
+            exit(missing_credentials);
+        _ ->
+            ok
+    end,
+
+    FinalUserVars = maps:merge(UserVars, #{amqp_user => AMQPUser, amqp_pass => AMQPPass}),
+
+    TcpPort = maps:get(port, FinalUserVars),
     {ok, _} = front_end:start_link([TcpPort]),
 
     lager:info("User vars: ~p", [UserVars]),
 
-    cvmfs_notify_sup:start_link([]).
+    cvmfs_notify_sup:start_link(FinalUserVars).
 
 %%--------------------------------------------------------------------
 stop(_State) ->
