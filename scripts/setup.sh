c9e9d3e53948fd22f7780686e39760e8849ddfce
Radu Popescu <mail@radupopescu.net>
2018-09-24 15:17:39 +0200

    Adding installation files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,10 +8,32 @@
 
 set -e
 
-#SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
+SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 
-DB_DIR=/var/lib/cvmfs-notify
+export RUNNER_LOG_DIR=/var/log/cvmfs-notify-runner
 
-echo "  - creating persistant storage directory at $DB_DIR"
-sudo mkdir -p $DB_DIR
-sudo chown -R `whoami` $DB_DIR
+# Install syslog configuration file
+if [ x"$(which systemctl)" != x"" ]; then
+    echo "  - restarting rsyslog"
+    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-notify-rotate-systemd /etc/logrotate.d/
+    sudo systemctl restart rsyslog
+
+    echo "  - installing systemd service file"
+    sudo cp -v $SCRIPT_LOCATION/cvmfs-notify.service /etc/systemd/system/cvmfs-notify.service
+    sudo systemctl daemon-reload
+else
+    echo "  - restarting rsyslog"
+    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-notify-rotate /etc/logrotate.d/
+    sudo service rsyslog restart
+fi
+
+# Symlink the configuration directory into /etc/cvmfs/notify
+if [ ! -e /etc/cvmfs/notify ]; then
+    echo "Creating onfiguration file directory to /etc/cvmfs/notify"
+    sudo mkdir /etc/cvmfs/notify
+fi
+if [ ! -e /etc/cvmfs/notify/config.json ]; then
+    echo "Copying config.json to /etc/cvmfs/notify"
+    sudo cp -v $SCRIPT_LOCATION/../etc/config.json /etc/cvmfs/notify/
+fi
+
