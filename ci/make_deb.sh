4a143716735dbe7b3918c4779ab8d1d4fdb2c985
Radu Popescu <mail@radupopescu.net>
2019-01-11 15:23:11 +0100

    Fix Deb packaging

--- ci/make_deb.sh
+++ ci/make_deb.sh
@@ -35,11 +35,9 @@ mkdir -p $WORKSPACE
 mkdir -p $WORKSPACE/usr/libexec/cvmfs-notify
 tar xzf ${BUILD_LOCATION}/$TARBALL -C $WORKSPACE/usr/libexec/cvmfs-notify
 
-mkdir -p $WORKSPACE/etc/{logrotate.d,rsyslog.d}
+mkdir -p $WORKSPACE/etc/logrotate.d
 cp -v ${BUILD_LOCATION}/scripts/90-cvmfs-notify-rotate-systemd \
     $WORKSPACE/etc/logrotate.d/
-cp -v ${BUILD_LOCATION}/scripts/90-cvmfs-notify.conf \
-    $WORKSPACE/etc/rsyslog.d/
 
 mkdir -p $WORKSPACE/etc/systemd/system
 cp -v ${BUILD_LOCATION}/scripts/cvmfs-notify.service \
@@ -56,10 +54,8 @@ fpm -s dir -t deb \
     --license "BSD-3-Clause" \
     --depends "cvmfs-server > 2.5.1" \
     --directories usr/libexec/cvmfs-notify \
-    --config-files etc/rsyslog.d/90-cvmfs-notify.conf \
     --config-files etc/logrotate.d/90-cvmfs-notify-rotate-systemd \
     --config-files etc/systemd/system/cvmfs-notify.service \
-    --exclude etc/rsyslog.d \
     --exclude etc/logrotate.d \
     --exclude etc/systemd/system \
     --no-deb-systemd-restart-after-upgrade \
