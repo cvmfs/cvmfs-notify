48370435dd3e78f15bc3cde56e146e11c789e690
Radu Popescu <mail@radupopescu.net>
2019-02-04 15:34:19 +0100

    Adjust install script for DEB pkg

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,16 +12,14 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 
 # Install syslog configuration file
  if [ x"$(which systemctl)" != x"" ]; then
-    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-notify-rotate-systemd /etc/logrotate.d/
-    sudo systemctl restart rsyslog
-    sudo cp $SCRIPT_LOCATION/cvmfs-notify.service /etc/systemd/system/cvmfs-notify.service
-    sudo systemctl daemon-reload
+    systemctl daemon-reload
+    systemctl restart rsyslog
 fi
 
 if [ ! -e /etc/cvmfs/notify ]; then
-    sudo mkdir /etc/cvmfs/notify
+    mkdir /etc/cvmfs/notify
 fi
 if [ ! -e /etc/cvmfs/notify/config.json ]; then
-    sudo cp $SCRIPT_LOCATION/../etc/config.json /etc/cvmfs/notify/
+    cp $SCRIPT_LOCATION/../etc/config.json /etc/cvmfs/notify/
 fi
 
