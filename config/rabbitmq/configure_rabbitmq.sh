cd46c4e9fab3119ab02a13f01b56c2ac6da4f906
Radu Popescu <mail@radupopescu.net>
2019-03-04 17:26:25 +0100

    Correct RabbitMQ provisioning scripts

--- config/rabbitmq/configure_rabbitmq.sh
+++ config/rabbitmq/configure_rabbitmq.sh
@@ -2,11 +2,8 @@
 
 admin_user=$1
 admin_pass=$2
-publisher_user=$3
-publisher_pass=$4
-subscriber_user=$5
-subscriber_pass=$6
-
+worker_user=$3
+worker_pass=$4
 
 # Enable management and last-value-cache exchange plugins
 rabbitmq-plugins enable rabbitmq_management
@@ -29,14 +26,8 @@ if [ x"$(rabbitmqctl list_users | grep '^${admin_user}' | wc -l)" = x"0" ]; then
 fi
 
 # Add and configure the publisher user
-if [ x"$(rabbitmqctl list_users | grep '^${publisher_user}' | wc -l)" = x"0" ]; then
-    rabbitmqctl add_user ${publisher_user} ${publisher_pass}
-    rabbitmqctl set_permissions -p /cvmfs ${publisher_user} "^(amq\.gen.*|repository\.activity)$" "^(amq\.gen.*|repository\.activity)$" ".*"
-fi
-
-# Add and configure the subscriber user
-if [ x"$(rabbitmqctl list_users | grep '^${subscriber_user}' | wc -l)" = x"0" ]; then
-    rabbitmqctl add_user ${subscriber_user} ${subscriber_pass}
-    rabbitmqctl set_permissions -p /cvmfs ${subscriber_user} "^(amq\.gen.*)$" "^(amq\.gen.*)$" ".*"
+if [ x"$(rabbitmqctl list_users | grep '^${worker_user}' | wc -l)" = x"0" ]; then
+    rabbitmqctl add_user ${worker_user} ${worker_pass}
+    rabbitmqctl set_permissions -p /cvmfs ${worker_user} "^(amq\.gen.*|repository\.activity)$" "^(amq\.gen.*|repository\.activity)$" ".*"
 fi
 
